b5b2d3afcdb66ce3a67dd579bbc75d7d02b83a85
WittBulter <nanazuimeng123@gmail.com>
2018-03-19 18:53:01 +0800

    feat(annotations): init optional

--- src/annotations/component.ts
+++ src/annotations/component.ts
@@ -1,34 +1,44 @@
 import Vue, { ComponentOptions } from 'vue'
-import * as Interface from '../interfaces'
-import { is, tools } from '../utils/index'
 import { VueConstructor } from 'vue/types/vue'
+import { metadata } from '../constants'
+import { tools } from '../utils'
 
 const componentFactory = (options?: ComponentOptions<Vue>) =>
-(component: new (...args: any[]) => any): VueConstructor<Vue> => {
-  const container: Interface.$Container = tools.findContainer()
-  const funcs: Array<new () => void> = tools.findConstructorParamTypes(component)
-  const params: string[] = tools.findConstructorParams(component)
-  const instances = container.saveInstance(funcs)
-  const datas = params.reduce((data, next, index) => Object.assign({}, data, { [next]: instances[index] }), {})
+(target: new (...args: any[]) => any): VueConstructor<Vue> => {
   
-  const prototype = component.prototype
-  const extras: any = Object.getOwnPropertyNames(component.prototype)
-    .reduce((tree, key) => {
-      const val = prototype[key]
-      const next = { [key]: val }
-      if (is.constructor(key)) return tools.assignChild(tree, 'methods', { test: () => val })
-      if (is.vueHook(key)) return Object.assign(tree, next)
-      if (is.vueMethod(key)) return tools.assignChild(tree, 'methods', next)
-      return tools.assignChild(tree, 'data', next)
-    }, { methods: {}, data: {} })
-  extras.data = Object.assign({}, extras.data, datas)
+  const types: any[] = Reflect.getMetadata(metadata.DESIGN_PARAM_TYPES, target)
   
-  const vueComponent = Object.assign({}, options, extras, {
-    data: () => extras.data,
-    template: '<p>123</p>',
-  })
-  const Extended: VueConstructor<Vue> = Vue.extend(vueComponent)
-  return Extended
+  Reflect.defineMetadata(metadata.HOST_PARAM_TYPES, types, target)
+  
+  Reflect.defineMetadata(metadata.COMPONENT_IDENTIFY, options || {}, target)
+  
+  // const container: Interface.$Container = tools.findContainer()
+  // const funcs: Array<new () => void> = tools.findConstructorParamTypes(component)
+  // const params: string[] = tools.findConstructorParams(component)
+  // const instances = container.saveInstance(funcs)
+  // const datas = params.reduce((data, next, index) => Object.assign({}, data, { [next]: instances[index] }), {})
+  //
+  // const prototype = component.prototype
+  // const extras: any = Object.getOwnPropertyNames(component.prototype)
+  //   .reduce((tree, key) => {
+  //     const val = prototype[key]
+  //     const next = { [key]: val }
+  //     if (is.constructor(key)) return tools.assignChild(tree, 'methods', { test: () => val })
+  //     if (is.vueHook(key)) return Object.assign(tree, next)
+  //     if (is.vueMethod(key)) return tools.assignChild(tree, 'methods', next)
+  //     return tools.assignChild(tree, 'data', next)
+  //   }, { methods: {}, data: {} })
+  // extras.data = Object.assign({}, extras.data, datas)
+  //
+  // const vueComponent = Object.assign({}, options, extras, {
+  //   data: () => extras.data,
+  //   template: '<p>123</p>',
+  // })
+  const Extended: VueConstructor<Vue> = Vue.extend({ template: '<p>123</p>' })
+  return (() => {
+    console.log(tools.findContainer())
+    return Extended
+  })()
 }
 
 export const Component = (options?: ComponentOptions<Vue>): (c: new (...args: any[]) => any)
