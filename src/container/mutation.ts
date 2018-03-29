42f1b32bb7a26a28507dad8116edc00abedef7c4
WittBulter <nanazuimeng123@gmail.com>
2018-03-29 15:00:59 +0800

    feat(mutation): add vue computed

--- src/container/mutation.ts
+++ src/container/mutation.ts
@@ -6,12 +6,14 @@ import { CollectionFactory, ContainerFactory, EuvComponentOptions } from '../int
 export type Extras = {
   methods: object,
   data: object,
+  computed: object,
   [key: string]: any,
 }
 
 export class Mutation {
   
   private _prototype: any
+  private _depDatas: { [key: string]: any } = {}
 
   constructor(
     private collection: CollectionFactory,
@@ -19,46 +21,52 @@ export class Mutation {
     private container: ContainerFactory,
   ) {
     this._prototype = collection.factory.prototype
+    this.updateDependencies()
   }
   
   toVueComponent(): VueConstructor<Vue> {
     const extras: Extras = Object.getOwnPropertyNames(this._prototype)
     .reduce((tree, key) => {
+      const descriptor: PropertyDescriptor = Reflect.getOwnPropertyDescriptor(this._prototype, key)
       const val: any = this._prototype[key]
       const next: { [key: string]: any } = { [key]: val }
+  
       if (is.constructor(key)) return tree
       if (is.vueHook(key)) return Object.assign(tree, next)
-      if (is.vueMethod(val)) return tools.assignChild(tree, 'methods', next)
+      if (is.vueMethod(descriptor)) return tools.assignChild(tree, 'methods', next)
+      if (is.vueComputed(descriptor)) return tools.assignChild(tree, 'computed', {
+        [key]: { get: descriptor.get, set: descriptor.set },
+      })
       return tools.assignChild(tree, 'data', next)
-    }, { methods: {}, data: {} })
-  
-    extras.data = () => Object.assign({}, extras.data, this.makeVueDatas())
+    }, { methods: {}, data: {}, computed: {} })
+    extras.data = () => Object.assign({}, extras.data, this._depDatas)
     
     return Vue.component(this.collection.bindingName, Object.assign({}, extras, this.makeVueExtra()))
-  }
-  
-  private makeVueDatas(): any {
-    const params: string[] = tools.findConstructorParams(this.collection.factory)
-    if (!params || !params.length) return {}
-    
-    return params
-    .reduce((data, next, index) => Object.assign({}, data, {
-      [next]: this.instances[index],
-    }), {})
   }
   
   private makeVueExtra(): any {
     const options: EuvComponentOptions = this.collection.vueComponentOptions || {}
     const components: object = (options.components || [])
     .reduce((coms, next) => {
-      const com = this.container.findOne(next).vueComponent
-      if (!com) return coms
-      return Object.assign({}, coms, { [next]: com })
+      const component: VueConstructor<Vue> = this.container.findOne(next).vueComponent
+      if (!component) return coms
+      return Object.assign({}, coms, { [next]: component })
     }, {})
+    
     return {
       template: options.template || '',
       name: this.collection.bindingName,
       components,
     }
+  }
+  
+  private updateDependencies(): void {
+    if (!this.instances || !this.instances.length) return
+    const shamFactory = new this.collection.factory(...this.instances)
+    
+    this._depDatas = Object.keys(shamFactory)
+    .reduce((pre, key) => Object.assign(pre, key && shamFactory[key] ? { [key]: shamFactory[key] } : {}), {})
+    
+    this._prototype = Object.assign(this._prototype, this._depDatas)
   }
 }
