1f2b9760060cbea726d1717d84297a001d09369a
WittBulter <nanazuimeng123@gmail.com>
2018-03-29 15:05:33 +0800

    feat(utils): remove unused

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,23 +1,9 @@
-import { ContainerFactory } from '../interfaces'
 
 export const tools = {
   assignChild: <T>(source: T, child: string, val: object): T => {
     return Object.assign({}, source, {
       [child]: Object.assign({}, source[child], val),
     })
-  },
-  
-  findConstructorParamTypes: (func: any): Array<new () => void> => {
-    return (<any>Reflect).getMetadata('design:paramtypes', func)
-  },
-  
-  findConstructorParams: (func: any): string[] => {
-    const result: RegExpExecArray = /\(\s*([\s\S]*?)\s*\)/.exec(func)
-    return result && result.length ? result[1].split(/\s*,\s*/) : []
-  },
-  
-  findContainer: (): ContainerFactory => {
-    return window.$Container
   },
 }
 
