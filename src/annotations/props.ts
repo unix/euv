a500dddfecff3c8a2af9620269c7ad59a4e17d7d
WittBulter <nanazuimeng123@gmail.com>
2018-04-02 16:21:39 +0800

    feat(props): init props annotation

---
+++ src/annotations/props.ts
@@ -0,0 +1,18 @@
+import { metadata } from '../constants'
+import { PropTagIdentifier } from '../interfaces'
+
+export const Props = () => (target: any, key: string, index?: number): void => {
+  
+  const prop: PropTagIdentifier = {
+    name: key,
+    type: Reflect.getMetadata(metadata.DESIGN_TYPE, target, key),
+    value: target[key],
+  }
+  
+  const props: PropTagIdentifier[] = Reflect.getMetadata(metadata.PROPS_IDENTIFIER, target) || []
+  
+  const next: PropTagIdentifier[] = props.concat(prop)
+  
+  Reflect.defineMetadata(metadata.PROPS_IDENTIFIER, next, target.constructor)
+  
+}
